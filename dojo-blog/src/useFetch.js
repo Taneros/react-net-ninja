6899690ae67ec3ea3fcf88b6e3f4b11116f2208f
Taneros <fricentaurus@GMAIL.COM>
2022-01-30 15:27:45 +0300

    featL finish react js net ninja

---
+++ dojo-blog/src/useFetch.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [url]);
+
+  return { data, isPending };
+};
+
+export default useFetch;
